d348e0de8e724ce54dc9988f691414a05c8692b8
modularity <Lauren.Dunlap@microsoft.com>
2020-11-02 22:14:59 -0800

    Update variables.tf

--- 101-terraform-job/terraform/variables.tf
+++ 101-terraform-job/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "appname" {
   type = string
   description = "Application name. Use only lowercase letters and numbers"
-  default = "starterterraform"
+  default = "starterterraform110220"
 }
 
 variable "environment" {
